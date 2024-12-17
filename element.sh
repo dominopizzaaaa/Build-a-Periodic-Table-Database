a761dd45df872c1c64df97a86b2f85df8d1fbb7c
dominopizzaaaa <94787443+dominopizzaaaa@users.noreply.github.com>
2024-12-17 23:58:54 +0800

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,37 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-# Check if an argument is provided
-if [[ -z $1 ]]; then
+if [[ -z $1 ]]
+then
   echo "Please provide an element as an argument."
   exit
 fi
 
-# Determine if the input is numeric or a string
-if [[ $1 =~ ^[0-9]+$ ]]; then
-  # Input is numeric (atomic number)
-  QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, 
-                p.melting_point_celsius, p.boiling_point_celsius
+# Check if input is a number
+if [[ $1 =~ ^[0-9]+$ ]]
+then
+  QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
          FROM elements e
          JOIN properties p ON e.atomic_number = p.atomic_number
          JOIN types t ON p.type_id = t.type_id
          WHERE e.atomic_number = $1;"
 else
-  # Input is string (symbol or name)
-  QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, 
-                p.melting_point_celsius, p.boiling_point_celsius
+  QUERY="SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
          FROM elements e
          JOIN properties p ON e.atomic_number = p.atomic_number
          JOIN types t ON p.type_id = t.type_id
          WHERE e.symbol = '$1' OR e.name = '$1';"
 fi
 
-ELEMENT_INFO=$($PSQL "$QUERY")
+RESULT=$($PSQL "$QUERY")
 
-if [[ -z $ELEMENT_INFO ]]; then
+if [[ -z $RESULT ]]
+then
   echo "I could not find that element in the database."
 else
-  IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT_INFO"
+  IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
   echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
 fi
